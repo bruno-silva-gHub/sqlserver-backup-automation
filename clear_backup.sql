3158c0cfe9ba5a6633760ad78f6b9c3694b34162
Bruno Silva <brunop300403@gmail.com>
2026-03-11 09:47:42 -0500

    Update clear_backup.sql

--- clear_backup.sql
+++ clear_backup.sql
@@ -1,5 +1,11 @@
-EXEC master.dbo.xp_delete_file
-0,
-N'C:\Backup\',
-N'bak',
-DATEADD(day,-30 ,GETDATE())
+DECLARE @DeleteDate DATETIME;
+SET @DeleteDate = DATEADD(day, -30, GETDATE());
+
+-- Para os arquivos na pasta BACKUP
+EXEC master.dbo.xp_delete_file 
+    0,                      
+    N'Disco de locação do backup',     
+    N'bak',                 
+    @DeleteDate, 
+    1;                      
+
